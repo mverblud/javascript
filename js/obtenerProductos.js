110067ec28bca8a0ae025b3d8f1b089871c7ba41
Marcos Verblud <marcosverblud@gmail.com>
2021-10-19 19:56:20 -0300

    Se agrega Jquery CND - archivos obtenerProductos/obtenerRedes

--- js/obtenerProductos.js
+++ js/obtenerProductos.js
@@ -1,69 +1,75 @@
-// Array de Productos
-const productos = [
-    {
-        id: 1,
-        codigo: '34301G-COR',
-        nombre: 'AMORTIGUADOR CHEVROLET CORSA 94/... DELANTERO',
-        imagen: 'imagenes/productos destacados/34301G.jpg',
-        precio: '3550'
-    },
-    {
-        id: 2,
-        codigo: '22181-COR',
-        nombre: 'AMORTIGUADOR PEUGEOT 404 NAFTA/DIESEL .../82 DELANTERO',
-        imagen: 'imagenes/productos destacados/22181.jpg',
-        precio: '2100'
-    },
-    {
-        id: 3,
-        codigo: '34302G-COR',
-        nombre: 'AMORTIGUADOR CHEVROLET CORSA 94/... DELANTERO',
-        imagen: 'imagenes/productos destacados/34301G.jpg',
-        precio: '4500'
-    },
-    {
-        id: 4,
-        codigo: '22182-COR',
-        nombre: 'AMORTIGUADOR PEUGEOT 404 NAFTA/DIESEL .../82 DELANTERO',
-        imagen: 'imagenes/productos destacados/22181.jpg',
-        precio: '1750'
+// jquery ready
+jQuery(() => {
+    // Array de Productos
+    const productos = [
+        {
+            id: 1,
+            codigo: '34301G-COR',
+            nombre: 'AMORTIGUADOR CHEVROLET CORSA 94/... DELANTERO',
+            imagen: 'imagenes/productos destacados/34301G.jpg',
+            precio: '3550'
+        },
+        {
+            id: 2,
+            codigo: '22181-COR',
+            nombre: 'AMORTIGUADOR PEUGEOT 404 NAFTA/DIESEL .../82 DELANTERO',
+            imagen: 'imagenes/productos destacados/22181.jpg',
+            precio: '2100'
+        },
+        {
+            id: 3,
+            codigo: '34302G-COR',
+            nombre: 'AMORTIGUADOR CHEVROLET CORSA 94/... DELANTERO',
+            imagen: 'imagenes/productos destacados/34301G.jpg',
+            precio: '4500'
+        },
+        {
+            id: 4,
+            codigo: '22182-COR',
+            nombre: 'AMORTIGUADOR PEUGEOT 404 NAFTA/DIESEL .../82 DELANTERO',
+            imagen: 'imagenes/productos destacados/22181.jpg',
+            precio: '1750'
+        }
+    ];
+
+    // Recupero carrito en localstorage
+    const productoLocalStorage = JSON.parse(localStorage.getItem("carrito"));
+    //const contenedorProductos  = document.getElementById("divProductos");
+
+    /*  Función para crear productos dinámicamente y crearlos en el contenedor */
+    const insertarProductos = () => {
+        for (const producto of productos) {
+            $('#divProductos').append(`
+                <div class="col mb-5" id="${producto.id}">
+                    <div class="card h-100 border-2 rounded-3 shadow-lg">
+                        <img class="card-img-top" src="${producto.imagen}" alt="">
+                        <div class="card-body p-4 border-top border-2">
+                            <div class="text-center">
+                                <h5 class="card-title">${producto.nombre}</h5>
+                                <p class="card-text">${producto.codigo}</p>
+                                <p class="card-text">$${producto.precio}</p>
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-lg btn-warning mt-auto" href="#">Agregar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`)
+
+            $(`#${producto.id}`).on("click", function () {
+                insertarProductosACanasta(producto);
+            });
+        }
     }
-];
 
-const productoLocalStorage = JSON.parse(localStorage.getItem("carrito"));
-const contenedorProductos  = document.getElementById("divProductos");
+    insertarProductos();
 
-/*  Función para crear productos dinámicamente y crearlos en el contenedor */
-const insertarProductos = () => {
-    for (const producto of productos) {
-        let cardProducto = document.createElement("div");
-        cardProducto.className = "col mb-5";
-        cardProducto.id = producto.id;
-        cardProducto.innerHTML = `
-        <div class="card h-100 border-2 rounded-3 shadow-lg">
-            <img class="card-img-top" src="${producto.imagen}" alt="">
-            <div class="card-body p-4 border-top border-2">
-                <div class="text-center">
-                    <h5 class="card-title">${producto.nombre}</h5>
-                    <p class="card-text">${producto.codigo}</p>
-                    <p class="card-text">$${producto.precio}</p>
-                </div>
-            </div>
-            <!-- Product actions-->
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center"><a class="btn btn-lg btn-warning mt-auto" href="#">Agregar</a>
-                </div>
-            </div>
-        </div>`;
-        cardProducto.onclick = () => insertarProductosACanasta(producto);
-        contenedorProductos.appendChild(cardProducto);
+    // Recupero carrito en localstorage
+    if (productoLocalStorage !== null) {
+        for (const producto of productoLocalStorage) {
+            insertarProductosACanasta(producto);
+        }
     }
-}
-
-insertarProductos();
-
-if (productoLocalStorage !== null) {
-    for (const producto of productoLocalStorage) {
-        insertarProductosACanasta(producto);
-    }
-}
+});
--- js/obtenerRedes.js
+++ js/obtenerRedes.js
@@ -1,43 +1,42 @@
-const redes = [
-    {
-        id: 1,
-        nombre: 'Whatsapp',
-        link: 'https://api.whatsapp.com/send?phone=543517687201&text=Hola%2C%20quiero%20hacer%20una%20consulta',
-        icon: 'fab fa-whatsapp'
-    },
-    {
-        id: 2,
-        nombre: 'Facebook',
-        link: 'https://web.facebook.com/ovsuspension/',
-        icon: 'fab fa-facebook-square'
-    },
-    {
-        id: 3,
-        nombre: 'Instagram',
-        link: 'https://www.instagram.com/ovsuspension',
-        icon: 'fab fa-instagram'
-    },
-    {
-        id: 4,
-        nombre: 'Google Maps',
-        link: 'https://g.page/ovsuspension?gm',
-        icon: 'fas fa-map-marker-alt'
+jQuery(() => {
+    const redes = [
+        {
+            id: 1,
+            nombre: 'Whatsapp',
+            link: 'https://api.whatsapp.com/send?phone=543517687201&text=Hola%2C%20quiero%20hacer%20una%20consulta',
+            icon: 'fab fa-whatsapp'
+        },
+        {
+            id: 2,
+            nombre: 'Facebook',
+            link: 'https://web.facebook.com/ovsuspension/',
+            icon: 'fab fa-facebook-square'
+        },
+        {
+            id: 3,
+            nombre: 'Instagram',
+            link: 'https://www.instagram.com/ovsuspension',
+            icon: 'fab fa-instagram'
+        },
+        {
+            id: 4,
+            nombre: 'Google Maps',
+            link: 'https://g.page/ovsuspension?gm',
+            icon: 'fas fa-map-marker-alt'
+        }
+    ];
+
+    /*  Función agregar redes sociales dinámicamente y crearlos en el contenedor */
+    const insertarRedes = () => {
+        for (const red of redes) {
+            $('#redSocial').append(`
+            <li id="${red.id}">
+                <a href="${red.link}" target="_Blank">
+                <i class="${red.icon}"></i>
+            </a>`);
+        }
     }
-];
 
-const listRedes = document.getElementById("redSocial");
+    insertarRedes(); ç
 
-/*  Función agregar redes sociales dinámicamente y crearlos en el contenedor */
-const insertarRedes = () => {
-    for (const red of redes) {
-        let contenidoRedSocial = document.createElement("li");
-        contenidoRedSocial.id = red.id;
-        contenidoRedSocial.innerHTML = `
-        <a href="${red.link}" target="_Blank">
-            <i class="${red.icon}"></i>
-        </a>`;
-        listRedes.appendChild(contenidoRedSocial);
-    }
-}
-
-insertarRedes();
+});
